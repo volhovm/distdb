c50a0383b7170dbba3fc8a93d2a9f46818935077
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-06-16 16:05:10 +0300

    Improve types, show/read instances

--- src/library/Types.hs
+++ src/library/Types.hs
@@ -1,7 +1,44 @@
 -- | Basic types common for the whole system
-module Types (Entry (..)) where
+module Types
+       ( Entry (..)
+       , Request (..)
+       , Response (..)
+       , responseMatches
+       ) where
 
 data Entry = Entry
     { eKey   :: String
     , eValue :: String
-    } deriving (Show, Eq, Ord)
+    } deriving (Eq,Ord)
+
+instance Show Entry where
+    show (Entry k v) = show k ++ " " ++ show v
+
+instance Read Entry where
+    readsPrec _ input = let (k:v:s) = words input in [(Entry k v, concat s)]
+
+data Request
+    = GetEntry String
+    | SetEntry Entry
+    | DeleteEntry String
+    | Ping
+    deriving (Show,Read,Eq)
+
+data Response
+    = EntryFound Entry
+    | EntrySet
+    | EntryDeleted
+    | EntryNotFound
+    | Pong
+    deriving (Show,Read,Eq)
+
+responseMatches :: Request -> Response -> Bool
+responseMatches rq rs =
+    case (rq, rs) of
+        (GetEntry _,EntryFound _)     -> True
+        (GetEntry _,EntryNotFound)    -> True
+        (SetEntry _,EntrySet)         -> True
+        (DeleteEntry _,EntryDeleted)  -> True
+        (DeleteEntry _,EntryNotFound) -> True
+        (Ping,Pong)                   -> True
+        _                             -> False
--- src/server/Journal.hs
+++ src/server/Journal.hs
@@ -2,12 +2,15 @@
 module Journal
        ( getEntries
        , putEntry
+       , getByKey
        , deleteByKey
        ) where
 
+import           Control.Monad          (void)
 import           Control.Monad.IO.Class (MonadIO (..), liftIO)
 import           Data.Function          (on)
-import           Data.List              (delete, find, nubBy)
+import           Data.List              (find, nubBy)
+import           Data.Maybe             (isJust)
 import           System.IO              (Handle, IOMode (ReadWriteMode),
                                          hGetContents, withFile)
 
@@ -15,33 +18,30 @@ import           Types                  (Entry (..))
 
 newtype Journal = Journal { journalPath :: FilePath }
 
-formatEntry :: Entry -> String
-formatEntry Entry{..} = eKey ++ " " ++ eValue
-
 withJournalFile :: (MonadIO m) => Journal -> (Handle -> IO a) -> m a
 withJournalFile Journal{..} = liftIO . withFile journalPath ReadWriteMode
 
 getEntries :: (MonadIO m) => Journal -> m [Entry]
 getEntries j = withJournalFile j $ \handle ->
-    map toEntry . lines <$> hGetContents handle
-  where
-    toEntry x = let (k:v:_) = words x in Entry k v
+    map read . lines <$> hGetContents handle
 
 overwriteJournal :: (MonadIO m) => Journal -> [Entry] -> m ()
 overwriteJournal j entries =
     liftIO $ writeFile (journalPath j) $
-        unlines $ map formatEntry $ nubBy ((==) `on` eKey) entries
+        unlines $ map show $ nubBy ((==) `on` eKey) entries
 
 getByKey :: (MonadIO m) => Journal -> String -> m (Maybe Entry)
 getByKey j k = find (\x -> eKey x == k) <$> getEntries j
 
 putEntry :: (MonadIO m) => Journal -> Entry -> m ()
 putEntry j e = do
-    deleteByKey j $ eKey e
-    liftIO $ appendFile (journalPath j) $ formatEntry e
+    void $ deleteByKey j $ eKey e
+    liftIO $ appendFile (journalPath j) $ show e
 
 -- | Deletes entry, returns [if the entry was there]
-deleteByKey :: (MonadIO m) => Journal -> String -> m ()
+deleteByKey :: (MonadIO m) => Journal -> String -> m Bool
 deleteByKey j k = do
     entries <- getEntries j
-    overwriteJournal j $ filter ((== k) . eKey) $ nubBy ((==) `on` eKey) entries
+    let isThere = isJust $ find ((== k) . eKey) entries
+    overwriteJournal j $ filter ((== k) . eKey) entries
+    return isThere
