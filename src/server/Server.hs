84e09c20e932ed47da0deb0c877aaf705428afb7
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-06-17 06:00:25 +0300

    Add pingpong-alike sync

--- src/server/Server.hs
+++ src/server/Server.hs
@@ -1,18 +1,21 @@
-{-# LANGUAGE BangPatterns              #-}
-{-# LANGUAGE DeriveGeneric             #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE RankNTypes                #-}
-{-# LANGUAGE ScopedTypeVariables       #-}
-{-# LANGUAGE ViewPatterns              #-}
+{-# LANGUAGE BangPatterns               #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE ExistentialQuantification  #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE ViewPatterns               #-}
 
 module Main where
 
 import           Control.Concurrent               (threadDelay)
 import qualified Control.Distributed.Backend.P2P  as P2P
 import           Control.Distributed.Process      (Process, ProcessId, expect,
-                                                   getSelfPid, processNodeId,
-                                                   receiveWait, register, say,
-                                                   send, spawnLocal)
+                                                   getSelfPid, match,
+                                                   processNodeId, receiveWait,
+                                                   register, say, send,
+                                                   spawnLocal)
 import           Control.Distributed.Process.Node (initRemoteTable,
                                                    newLocalNode, runProcess)
 import           Network.Socket                   (withSocketsDo)
@@ -20,9 +23,13 @@ import           Network.Transport.TCP            (createTransport,
                                                    defaultTCPParameters)
 
 import           Control.Exception                (bracket, catch)
+import Control.Lens
+       (makeLenses, use, view, (&), (.~), (.=), (+=), uses)
 import           Control.Monad                    (forM_, forever, void, when)
 import           Control.Monad.IO.Class           (liftIO)
-import           Control.Monad.RWS                (RWS (..), execRWS)
+import           Control.Monad.RWS.Strict         (MonadReader, MonadState,
+                                                   MonadWriter, RWS (..), ask,
+                                                   execRWS, tell)
 import           Data.Bifunctor                   (bimap)
 import           Data.Binary                      (Binary (..))
 import           Data.List                        (delete, (\\))
@@ -40,58 +47,101 @@ import           Types                            (EntryRequest (..),
                                                    NetworkConfig (..),
                                                    Pinging (..), Port (getPort))
 
-class (Binary a, Typeable a) => SendableLike a where
+-- Explicitely typed message
+data Message a = Message
+    { msgFrom :: ProcessId
+    , msgBody :: a
+    } deriving (Show,Generic,Typeable)
+
+instance (Binary a) => Binary (Message a)
+
+class (Binary a, Show a, Typeable a) => SendableLike a where
     send' :: ProcessId -> a -> Process ()
-    send' pid x = send pid x
+    send' pid x = send pid $ Message pid x
 
 instance SendableLike Pinging
 instance SendableLike EntryRequest
 
 data Sendable = forall a . SendableLike a => Sendable { unSendable :: a }
 
+instance Show Sendable where
+    show (Sendable a) = "Sendable wrapper of: { " ++ show a ++ " }"
+
 data ServerConfig = ServerConfig
-    { serverID   :: ProcessId
+    { serverPid  :: ProcessId
     , serverHost :: Host
     , serverPort :: Port
     , serverId   :: Int
-    }
-
-data ServerState = ServerState
-    { isLeader :: Bool
     } deriving (Show)
 
-data Message = Message
-   { messageFrom :: ProcessId
-   , messageTo   :: ProcessId
-   , msg         :: Sendable
-   }
+data ServerState = ServerState
+    { _isLeader    :: Bool
+    , _pongsNumber :: Int
+    , _peers       :: [ProcessId]
+    } deriving (Show)
+makeLenses ''ServerState
 
-newtype ServerM a = ServerM { runServerM :: RWS ServerConfig [Message] ServerState a }
+-- Polymorphic message
+data PolyMessage = PolyMessage
+    { msgTo' :: ProcessId
+    , msgBody' :: Sendable
+    } deriving (Show)
+
+newtype ServerM a = ServerM
+    { runServerM :: RWS ServerConfig [PolyMessage] ServerState a
+    } deriving (Functor,Applicative,Monad,MonadState ServerState,
+                MonadWriter [PolyMessage],MonadReader ServerConfig)
 
 data Tick = Tick deriving (Show,Generic,Typeable)
 instance Binary Tick
 
 threadDelayMS s = threadDelay $ s * (1000 :: Int)
 say' = liftIO . putStrLn
+--
+--getOthers :: Process [ProcessId]
+--getOthers = do
+--    pid <- processNodeId <$> getSelfPid
+--    peers <- P2P.getPeers
+--    return $ delete pid peers
+
+pingingHandler :: Message Pinging -> ServerM ()
+pingingHandler (Message from Ping) = do
+    ServerConfig{..} <- ask
+    void $ tell [PolyMessage from $ Sendable Pong]
+pingingHandler (Message from Pong) = do
+    pongsNumber += 1
+
+tickHandler :: Tick -> ServerM ()
+tickHandler _ = do
+    curPeers <- use peers
+    peersSize <- uses peers length
+    pongsLess <- uses pongsNumber $ (< peersSize)
+    when (pongsLess) $ traceM "Not all pongs were here))"
+    pongsNumber .= 0
+    void $ tell $ map (\p -> PolyMessage p $ Sendable Ping) curPeers
 
 runServer :: ServerConfig -> ServerState -> Process ()
 runServer config state = do
     let run handler msg = return $ execRWS (runServerM $ handler msg) config state
-    (state' :: ServerState, sendMessages :: [Message]) <- receiveWait []
+    (state' :: ServerState, sendMessages :: [PolyMessage]) <-
+        receiveWait [ match $ run tickHandler
+                    , match $ run pingingHandler ]
     say' $ "Current state: " ++ show state'
-    forM_ sendMessages $ \Message{..} -> case msg of
-        (Sendable a) -> send' messageTo a
-    runServer config state'
+    forM_ sendMessages $ \PolyMessage{..} -> case msgBody' of
+                             (Sendable a) -> send' msgTo' a
+    newPeers <- getNodes
+    runServer config $ state' & peers .~ newPeers
 
 worker :: Int -> (Host, Port) -> NetworkConfig -> Process ()
 worker index (host, port) conf@NetworkConfig{..} = do
     liftIO $ threadDelayMS 2000
-    getSelfPid >>= register (show $ getPort port)
+    getSelfPid >>= register "distdbNode"
     myPid <- getSelfPid
     void $ spawnLocal $ forever $ do
-        liftIO $ threadDelayMS 5
+        liftIO $ threadDelayMS 8000
         send myPid Tick
-    runServer (ServerConfig myPid host port index) (ServerState $ index == 1)
+    nodes <- getNodes
+    runServer (ServerConfig myPid host port index) (ServerState (index == 1) 0 nodes)
 --    pids <- getOthers
 --    forever $ do
 --        when (port == "3551") $ do
@@ -103,10 +153,10 @@ worker index (host, port) conf@NetworkConfig{..} = do
 --        liftIO $ threadDelayMS $ if port == "3551" then 500 else 3000
 --  where
 
-getOthers = do
-    pid <- processNodeId <$> getSelfPid
-    peers <- P2P.getPeers
-    return $ delete pid peers
+getNodes :: Process [ProcessId]
+getNodes = do
+    self <- getSelfPid
+    delete self <$> P2P.getCapable "distdbNode"
 
 main :: IO ()
 main = do
